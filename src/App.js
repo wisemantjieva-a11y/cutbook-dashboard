dbec1646768078ca960d84194562c410b985b0fc
wisemantjieva-a11y <wisemantjieva@gmail.com>
2026-06-08 12:47:32 +0200

    Update import paths for Home and Register components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home.jsx';
-import Register from './Register.jsx';
+import Home from './pages/Home.jsx';
+import Register from './pages/Register.jsx';
 
 function App() {
   return (
