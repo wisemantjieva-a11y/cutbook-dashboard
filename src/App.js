690da192c17aa42b4ca3bdc6d7e6b17035af44ff
wisemantjieva-a11y <wisemantjieva@gmail.com>
2026-06-08 13:07:35 +0200

    Change import file extensions from .js to .jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Register from './pages/Register';
+import Home from './pages/Home.jsx';
+import Register from './pages/Register.jsx';
 
 function App() {
   return (
