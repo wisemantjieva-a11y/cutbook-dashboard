802340500673c4796e1cd7f1c106009b9ec8c141
wisemantjieva-a11y <wisemantjieva@gmail.com>
2026-06-08 11:55:33 +0200

    Refactor App component to use React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import OwnerApp from './pages/OwnerApp';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home.jsx';
+import Register from './Register.jsx';
 
 function App() {
-  return <OwnerApp />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
